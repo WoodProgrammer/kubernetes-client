942d5721b67864827bbfa1e661eada3c0cf32af1
Emir Özbir <emirozbir@hotmail.com>
2018-08-01 01:25:45 +0300

    Configurer Tests written all tests passed

--- src/configure.rb
+++ src/configure.rb
@@ -1,10 +1,22 @@
+require "http"
+
 class Configure
 
+  attr_reader :kubernetesUrl
+
+  def initialize
+    @kubernetesUrl = ENV["kubernetesUrl"]
+  end
+
+
+  def getKubernetesUrl
+    return @kubernetesUrl
+  end
 
   def connectionProxy
 
-
-    
+    statusCode = HTTP.get(@kubernetesUrl.to_s).status
+    return statusCode
 
   end
 end
--- tests/test_configure.rb
+++ tests/test_configure.rb
@@ -3,12 +3,16 @@ require "test/unit"
 
 class TestConfigureClass < Test::Unit::TestCase
 
+  def testgetKubernetesUrl
+    @configureObj = Configure.new
+
+    assert_equal("https://127.0.0.1:8080/", @configureObj.getKubernetesUrl())
+  end
 
   def testConnection
     @configureObj = Configure.new
-
+    
     assert_equal(200, @configureObj.connectionProxy())
-
   end
 
 end
